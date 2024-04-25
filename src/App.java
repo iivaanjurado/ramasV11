af1ed450697e4b2474c4b3ada4c2b35e6f29a292
iivaanjurado <ija0001@alu.medac.es>
2024-04-25 10:17:26 +0200

    develop->develop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("main->develop");
+        System.out.println("develop->develop");
     }
 }
