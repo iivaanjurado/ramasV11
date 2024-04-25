6be01451a2311c365c5968c4d10ab129199ffdee
iivaanjurado <ija0001@alu.medac.es>
2024-04-25 10:16:43 +0200

    feature2->feature2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("main->develop");
         System.out.println("feature2->feature2");
+        System.out.println("feature2->feature2");
     }
 }
