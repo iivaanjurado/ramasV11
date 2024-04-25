c94cfafbf8cfbc343e9f68212eb3fc0e7a7a02f5
iivaanjurado <ija0001@alu.medac.es>
2024-04-25 10:27:59 +0200

    develop->develop

--- src/App.java
+++ src/App.java
@@ -1,12 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
         System.out.println("main->develop");
         System.out.println("develop->develop");
         System.out.println("develop->develop");
-=======
-        System.out.println("Hello, World!");
-        System.out.println("main->hotfix");
->>>>>>> hotfix
+        System.out.println("develop->develop");
     }
 }
